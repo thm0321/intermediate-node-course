adab0af53a9c0b899d84fe110fa1fe2d9e315714
Thomas Mendola <thomasmendola@Thomass-Air.home>
2020-08-30 14:01:12 -0400

    add created create user route

--- server.js
+++ server.js
@@ -15,7 +15,22 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create({
+    name:req.body.newData.name,
+    email:req.body.newData.email, 
+    password:req.body.newData.password
+  },
+  (err, data)=>{
+    if(err){
+      res.json({success:false,message:err})
+    }
+    else if(!data){
+      res.json({success:false,message:"Not Found"})
+    }
+    else{
+      res.json({success:true,data:data})
+    }
+  })
 })
 
 app.route('/users/:id')
